aab2c1232f115600703fec2757134da225e42297
Oskar Grzelak <oskargrzelak06@gmail.com>
2023-12-22 22:03:05 +0100

    Added DOMInteractions, connected DOM elements and setup listeners, added dropdown-menu handling, deleted semicolons

---
+++ src/app.js
@@ -0,0 +1,41 @@
+import { getShowsByKey, getShowById } from "./requests.js"
+import { mapListToDOMElements } from "./DOMInteractions.js"
+
+class TvApp {
+    constructor() {
+        this.viewElems = {}
+        this.showNameButtons = {}
+        this.selectedName = "harry"
+        this.initializeApp()
+    }
+
+    initializeApp = () => {
+        this.connectHTMLElements()
+        this.setupListeners()
+    }
+
+    connectHTMLElements = () => {
+        const listOfIds = Array
+            .from(document.querySelectorAll("[id]"))
+            .map(elem => elem.id)
+
+        const listOfShowNames = Array
+            .from(document.querySelectorAll("[data-show-name]"))
+            .map(elem => elem.dataset.showName)
+
+        this.viewElems = mapListToDOMElements(listOfIds, "id")
+        this.showNameButtons = mapListToDOMElements(listOfShowNames, "data-show-name")
+    }
+
+    setupListeners = () => {
+        Object.keys(this.showNameButtons).forEach(showName => {
+            this.showNameButtons[showName].addEventListener('click', this.setCurrentNameFilter)
+        })
+    }
+
+    setCurrentNameFilter = event => {
+        this.selectedName = event.target.dataset.showName
+    }
+}
+
+document.addEventListener('DOMContentLoaded', new TvApp)
